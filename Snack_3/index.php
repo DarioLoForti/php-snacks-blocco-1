40ac1ce762f3c52a77e377d9430c5289c3fea461
DarioLoForti <lofortidario@gmail.com>
2024-01-24 17:27:48 +0100

    snak 3 completato

--- Snack_3/index.php
+++ Snack_3/index.php
@@ -5,43 +5,43 @@ $posts = [
     '20/01/2024' => [
         [
             'fonte' => 'Corriere della Sera',
-            'title' => 'Biden, gelo con Netanyahu',
-            'author' => 'Monica Colombo',
-            'text' => 'Telefonata di 40 minuti: "Servono due Stati". Lite nel governo di Israele'
+            'titolo' => 'Biden, gelo con Netanyahu',
+            'autore' => 'Monica Colombo',
+            'testo' => 'Telefonata di 40 minuti: "Servono due Stati". Lite nel governo di Israele'
         ],
         [
             'fonte' => 'La repubblica',
-            'title' => 'Italia in vendita',
-            'author' => 'Eugenio Lupo',
-            'text' => "Il governo punta a fare cassa vendendo quote dell'azienda di Stato: Eni, Poste, Mps, Ferrovie. Obiettivo 20 miliardi"
+            'titolo' => 'Italia in vendita',
+            'autore' => 'Eugenio Lupo',
+            'testo' => "Il governo punta a fare cassa vendendo quote dell'azienda di Stato: Eni, Poste, Mps, Ferrovie. Obiettivo 20 miliardi"
         ],
     ],
     '21/01/2024' => [
         [
             'fonte' => 'La Stampa',
-            'title' => 'Israele-Iran, scontro totale. ',
-            'author' => 'Riccardo Rospetti',
-            'text' => 'Onu e Usa in pressing su Bibi'
+            'titolo' => 'Israele-Iran, scontro totale. ',
+            'autore' => 'Riccardo Rospetti',
+            'testo' => 'Onu e Usa in pressing su Bibi'
         ],
         [
             'fonte' => 'Gazzetta dello Sport',
-            'title' => 'Il coraggio del Diavolo ',
-            'author' => 'Giuseppe Bianchini',
-            'text' => 'Cori razzisti: Maignan se ne va, il Milan vince al 93.'
+            'titolo' => 'Il coraggio del Diavolo ',
+            'autore' => 'Giuseppe Bianchini',
+            'testo' => 'Cori razzisti: Maignan se ne va, il Milan vince al 93.'
         ]
     ],
     '22/01/2024' => [
         [
-            'fonte' => 'Sky tg 24',
-            'title' => 'Guerra Ucraina-Russia. Tusk a Kiev: aiuti da Polonia, conflitto è lotta bene-male',
-            'author' => 'Pietro Tremoli',
-            'text' => "La Polonia fornirà un nuovo pachetto di aiuti all'Ucraina. Lo ha annunciato il presidente ucraino Zelensky incontrando a Kiev il premier polacco Donald Tusk."
+            'fonte' => 'Sky Tg24',
+            'titolo' => 'Guerra Ucraina-Russia. Tusk a Kiev: aiuti da Polonia, conflitto è lotta bene-male',
+            'autore' => 'Pietro Tremoli',
+            'testo' => "La Polonia fornirà un nuovo pachetto di aiuti all'Ucraina. Lo ha annunciato il presidente ucraino Zelensky incontrando a Kiev il premier polacco Donald Tusk."
         ],
         [
             'fonte' => 'Corriere della Sera',
-            'title' => "Mar Rosso, l'Italia si schiera.",
-            'author' => 'Monica Colombo',
-            'text' => '"Missione difensiva" con Francia e Germania. Netanyahu sotto assedio anche a casa'
+            'titolo' => "Mar Rosso, l'Italia si schiera.",
+            'autore' => 'Monica Colombo',
+            'testo' => '"Missione difensiva" con Francia e Germania. Netanyahu sotto assedio anche a casa'
         ]        
     ]
 ];
@@ -58,6 +58,28 @@ $posts = [
     <title>php-snacks-blocco-3</title>
 </head>
 <body>
+    <div class="bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1 class="my-4 text-white">News</h1>
+                </div>
+                <?php foreach ($posts as $key => $post) { ?>
+                    <h2 class="my-5 text-white">News del: <?php echo $key ?></h2>
     
+                    <?php foreach ($post as $news_post) {  ?>
+                            <div class="col-6">
+                                <div class="container-post">
+                                    <h5 class=" py-2">Fonte: <?php echo $news_post['fonte']; ?> </h5>
+                                    <h3 class=" text-center py-2  "><?php echo $news_post['titolo'];  ?></h3>
+                                    <h6 class=" text-center py-2"><?php echo $news_post['testo'];  ?></h6>
+                                    <h6 class="text-end py-2">Autore: <?php echo $news_post['autore'];  ?></h6>
+                                </div>
+                            </div>
+                        <?php } ?>
+                <?php } ?>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
