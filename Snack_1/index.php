f6926f037a00a2736e251077725338e441b1bb29
DarioLoForti <lofortidario@gmail.com>
2024-01-24 16:43:12 +0100

    snack 1 completato

--- Snack_1/index.php
+++ Snack_1/index.php
@@ -16,7 +16,7 @@ $matches = [
     ],
     [
         'home' => 'Happy Casa Brindisi',
-        'guest' => 'generazione Vincente Napoli Basket',
+        'guest' => 'Generazione Vincente Napoli Basket',
         'home_points' => 77,
         'guest_points' => 80
     ],
@@ -61,12 +61,29 @@ $matches = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php-snacks-blocco-1</title>
-    <link rel="stylesheet" href="../style/style.css">
+    <link rel="stylesheet" href="../css/style.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 
-<body>
-
+<body class="bg">
+    <main>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1 class="text-white fw-bold pt-3">LBA Serie A giornata 17°:</h1>
+                </div>
+                <?php foreach ($matches as $match) {  ?>
+                    <div class="col-6 py-5">
+                        <div class="container-match">
+                            <h3 class=" text-center "><?php echo $match['home']." - "."vs"." - ".$match['guest'];  ?></h3>
+                            <h5 class="text-center py-1"> Finale: </h5>
+                            <h6 class="text-center"><?php echo $match['home_points']." - ".$match['guest_points'];  ?></h6>
+                        </div>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    </main>
 </body>
 
 </html>
