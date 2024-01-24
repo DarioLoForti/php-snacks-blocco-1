576c6c5f4d56b338bdd83714ab7700fa69219127
DarioLoForti <lofortidario@gmail.com>
2024-01-24 16:27:49 +0100

    add array squadre

--- Snack 1/index.php
+++ Snack 1/index.php
@@ -1,11 +1,72 @@
+<?php
+
+// definisco l'array della giornata di campionato, con squadre sfidanti e punteggio della partita
+$matches = [
+    [
+        'home' => 'Dolomiti Energia Trentino',
+        'guest' => 'Germani Brescia',
+        'home_points' => 69,
+        'guest_points' => 93
+    ],
+    [
+        'home' => 'Estra Pistoia',
+        'guest' => 'Givova Scafati Basket',
+        'home_points' => 78,
+        'guest_points' => 71
+    ],
+    [
+        'home' => 'Happy Casa Brindisi',
+        'guest' => 'generazione Vincente Napoli Basket',
+        'home_points' => 77,
+        'guest_points' => 80
+    ],
+    [
+        'home' => 'Openjobmetis Varese',
+        'guest' => 'Virtus segafredo Bologna',
+        'home_points' => 69,
+        'guest_points' => 81
+    ],
+    [
+        'home' => 'Bertram derthona Tortona',
+        'guest' => 'Umana Reyer venezia',
+        'home_points' => 75,
+        'guest_points' => 69
+    ],
+    [
+        'home' => 'Unahotels Reggio Emilia',
+        'guest' => 'Vanoli Basket Cremona',
+        'home_points' => 78,
+        'guest_points' => 71
+    ],
+    [
+        'home' => 'Carpegna Prosciutto Pesaro',
+        'guest' => 'EA7 Emporio Armani Milano',
+        'home_points' => 65,
+        'guest_points' => 85
+    ],
+    [
+        'home' => 'NutriBullet Treviso Basket',
+        'guest' => 'Banco di Sardegna Sassari',
+        'home_points' => 77,
+        'guest_points' => 70
+    ],
+]
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-snacks-blocco-1</title>
+    <link rel="stylesheet" href="../style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
+
 <body>
-    
+
 </body>
+
 </html>
