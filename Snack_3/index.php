6c18456622e3727951dcd4ef7d763f840cdf39cf
DarioLoForti <lofortidario@gmail.com>
2024-01-24 17:04:01 +0100

    add array post

---
+++ Snack_3/index.php
@@ -0,0 +1,63 @@
+<?php
+// definisco array di post
+$posts = [
+
+    '20/01/2024' => [
+        [
+            'fonte' => 'Corriere della Sera',
+            'title' => 'Biden, gelo con Netanyahu',
+            'author' => 'Monica Colombo',
+            'text' => 'Telefonata di 40 minuti: "Servono due Stati". Lite nel governo di Israele'
+        ],
+        [
+            'fonte' => 'La repubblica',
+            'title' => 'Italia in vendita',
+            'author' => 'Eugenio Lupo',
+            'text' => "Il governo punta a fare cassa vendendo quote dell'azienda di Stato: Eni, Poste, Mps, Ferrovie. Obiettivo 20 miliardi"
+        ],
+    ],
+    '21/01/2024' => [
+        [
+            'fonte' => 'La Stampa',
+            'title' => 'Israele-Iran, scontro totale. ',
+            'author' => 'Riccardo Rospetti',
+            'text' => 'Onu e Usa in pressing su Bibi'
+        ],
+        [
+            'fonte' => 'Gazzetta dello Sport',
+            'title' => 'Il coraggio del Diavolo ',
+            'author' => 'Giuseppe Bianchini',
+            'text' => 'Cori razzisti: Maignan se ne va, il Milan vince al 93.'
+        ]
+    ],
+    '22/01/2024' => [
+        [
+            'fonte' => 'Sky tg 24',
+            'title' => 'Guerra Ucraina-Russia. Tusk a Kiev: aiuti da Polonia, conflitto è lotta bene-male',
+            'author' => 'Pietro Tremoli',
+            'text' => "La Polonia fornirà un nuovo pachetto di aiuti all'Ucraina. Lo ha annunciato il presidente ucraino Zelensky incontrando a Kiev il premier polacco Donald Tusk."
+        ],
+        [
+            'fonte' => 'Corriere della Sera',
+            'title' => "Mar Rosso, l'Italia si schiera.",
+            'author' => 'Monica Colombo',
+            'text' => '"Missione difensiva" con Francia e Germania. Netanyahu sotto assedio anche a casa'
+        ]        
+    ]
+];
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>php-snacks-blocco-3</title>
+</head>
+<body>
+    
+</body>
+</html>
